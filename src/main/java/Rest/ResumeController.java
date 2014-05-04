38584db7d2d0f9b473c2acf1a4b76926b5cb3df2
Yoanlibert <yirachan@live.fr>
2014-05-04 04:19:06 +0100

    [projet] : GET / test

--- src/main/java/Rest/ResumeController.java
+++ src/main/java/Rest/ResumeController.java
@@ -22,7 +22,7 @@ public class ResumeController {
      * La liste des resumes
      */
 
-   /* public void addResume(){
+   public void addResume(){
         Adresse adresseList = new Adresse();
         adresseList.addAdresses("37 rue du Maréchal Juin, 76130 Mont Saint Aignan", "Rouen, France");
 
@@ -61,7 +61,7 @@ public class ResumeController {
         list.resumes.add(resumes2);
         list.resumes.add(resumes3);
 
-    }*/
+    }
 
     /**
      * Get avec un identifiant
@@ -111,44 +111,6 @@ public class ResumeController {
     @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody
     ResumeList getResumeInXML(){
-
-        Adresse adresseList = new Adresse();
-        adresseList.addAdresses("37 rue du Maréchal Juin, 76130 Mont Saint Aignan", "Rouen, France");
-
-        ExperciencePro mesexpList = new ExperciencePro();
-        mesexpList.addExperiencePro("Vente de matériel informatique chez SysInfo", "4 mois");
-        mesexpList.addExperiencePro("Développement des applications chez EVE", "3 mois");
-        mesexpList.addExperiencePro("Web master chez une boite de communication", "3 mois");
-
-        Scolarites scolaritesList = new Scolarites();
-        scolaritesList.addScolarites("Collège Camille Saint-Saens", "3 ans");
-        scolaritesList.addScolarites("Lycée Pierre Corneille", "3 ans");
-        scolaritesList.addScolarites("UFR des sciences et techniques, Rouen", "4 ans");
-
-        Langues languesList = new Langues();
-        languesList.addLangues("Berbère", "Langue maternelle");
-        languesList.addLangues("Français", "Avancé");
-        languesList.addLangues("Arabe", "Avancé");
-        languesList.addLangues("Anglais", "Avancé");
-
-        CompetenceInfo competenceInfoList = new CompetenceInfo();
-        competenceInfoList.addCompetencesInfo("Languages de programmation : Java, PHP, Flash", "Avancé");
-        competenceInfoList.addCompetencesInfo("Conception : UML", "Avancé");
-        competenceInfoList.addCompetencesInfo("Base de données : Oracle", "Avancé");
-
-        Resume resumes1 = new Resume("AMARA", "Safia", "10-10-1990", "07.53.62.66.35", adresseList,
-                "Jeune étudiante avec Bac+4 en phase de démmarage souhaitant evoluer et gagner de l'expérience.",
-                mesexpList, scolaritesList, "", languesList, competenceInfoList);
-        Resume resumes2 = new Resume("LIBERT", "Yoan", "07-07-1989", "06.61.57.76.59", adresseList,
-                "Etudiant en Informatique souhaitant evoluer dans le domaine du développement.",
-                mesexpList, scolaritesList, "Interprète pour les langues : Kabyle, Anglais", languesList, competenceInfoList);
-        Resume resumes3 = new Resume("MOUGEN", "Ashtaka", "17-01-1990", "07.53.77.77.62", adresseList,
-                "Tester le marché de travail afin d'avoir de l'expérience.",
-                mesexpList, scolaritesList, "Vente et achat", languesList, competenceInfoList);
-
-        list.resumes.add(resumes1);
-        list.resumes.add(resumes2);
-        list.resumes.add(resumes3);
 
         return list;
     }
